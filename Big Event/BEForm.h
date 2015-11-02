3336ab5eb562eb7a8a45b50081852bfc25ddca33
Jonathan Willing <j@jwilling.com>
2015-11-01 18:11:39 -0600

    add PonyDebugger for request debugging

---
+++ Big Event/BEForm.h
@@ -0,0 +1,13 @@
+//
+//  BEForm.h
+//  Big Event
+//
+//  Created by Jonathan Willing on 10/29/15.
+//  Copyright © 2015 Big Event. All rights reserved.
+//
+
+#import <Mantle/Mantle.h>
+
+@interface BEForm : MTLModel
+
+@end
--- Podfile
+++ Podfile
@@ -2,5 +2,6 @@ pod 'XLForm', '~> 3.0.2'
 pod 'UICKeyChainStore'
 pod 'SVProgressHUD', :head
 pod 'Mantle'
+pod 'PonyDebugger'
 
 inhibit_all_warnings!
